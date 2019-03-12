c703c5f427e3ec421a70c8917b306b70c4dd9804
zero <tobewhatwewant@gmail.com>
2019-03-12 16:40:41 +0800

    :label:(@zodash/pick): update pick return type with PartialDeep<T>

--- packages/pick/src/pick.ts
+++ packages/pick/src/pick.ts
@@ -1,3 +1,7 @@
+type PartialDeep<T> = {
+  [P in keyof T]?: PartialDeep<T[P]>;
+};
+
 /**
  * Create an object composed of the picked `object` properties.
  * 
@@ -5,7 +9,7 @@
  * @param keys the property keys to pick.
  * @returns the new object.
  */
-export function pick<T extends object>(value: T, keys: string[]) {
+export function pick<T extends object>(value: T, keys: string[]): PartialDeep<T> {
   return keys.reduce((p, k) => {
     if (k in value) {
       p[k] = value[k];
