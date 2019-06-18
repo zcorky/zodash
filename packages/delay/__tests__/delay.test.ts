aa6662d7f086029483e5d6598b37c4a3ac3a0ad0
whatwewant <tobewhatwewant@gmail.com>
2019-06-18 18:34:19 +0800

    :sparkles:(packages/delay): realize delay

---
+++ packages/delay/__tests__/delay.test.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+import { deepEqual } from "@zcorky/deep-equal";
+import { delay } from "../src/delay";
+
+describe("@zodash/delay", () => {
+  it("same as builtin delay", () => {
+    
+  });
+});
