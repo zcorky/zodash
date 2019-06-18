3d821ca4e4b81bdab7304ab17986944a28e32cf1
whatwewant <tobewhatwewant@gmail.com>
2019-06-18 18:34:40 +0800

    :sparkles:(packages/debounce): realize debounce

---
+++ packages/debounce/__tests__/debounce.test.ts
@@ -0,0 +1,9 @@
+"use strict";
+
+import { deepEqual } from "@zcorky/deep-equal";
+import { debounce } from "../src/debounce";
+
+describe("@zodash/debounce", () => {
+  it("common", () => {
+  });
+});
