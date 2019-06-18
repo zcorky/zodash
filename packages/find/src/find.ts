c75a98f6c68a93ee78b22210ee6a4e4ffd4745e2
whatwewant <tobewhatwewant@gmail.com>
2019-06-18 15:28:29 +0800

    :sparkles:(packages/find): add find

---
+++ packages/find/src/find.ts
@@ -0,0 +1,9 @@
+/**
+ * find an object from a list
+ * 
+ * @param list value list
+ * @param fn find function
+ */
+export function find<T>(list: T[], fn: (one: T, index: number, originList: T[]) => boolean) {
+  return list.find(fn);
+}
