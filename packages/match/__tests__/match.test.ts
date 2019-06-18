fe631b0bbd36a1e2ffc64d6e690e83f2aa17673c
whatwewant <tobewhatwewant@gmail.com>
2019-06-18 18:32:55 +0800

    :sparkles:(packages/match): add match => reducer

---
+++ packages/match/__tests__/match.test.ts
@@ -0,0 +1,50 @@
+"use strict";
+
+import { deepEqual } from "@zcorky/deep-equal";
+import { match } from "../src/match";
+
+describe("@zodash/match", () => {
+  it("common", () => {
+    interface Data {
+      x: number;
+      y: number;
+      method: '+' | '-' | '*' | '/';
+    }
+
+    const handlers = {
+      '+': (data: Data) => data.x + data.y,
+      '-': (data: Data) => data.x - data.y,
+      '*': (data: Data) => data.x * data.y,
+      '/': (data: Data) => data.x / data.y,
+    };
+
+    expect(deepEqual(match({ method: '+', x: 1, y: 2 }, handlers, (d) => d['method']), 3)).toBeTruthy();
+    expect(deepEqual(match({ method: '-', x: 1, y: 2 }, handlers, (d) => d['method']), -1)).toBeTruthy();
+    expect(deepEqual(match({ method: '*', x: 1, y: 2 }, handlers, (d) => d['method']), 2)).toBeTruthy();
+    expect(deepEqual(match({ method: '/', x: 1, y: 2 }, handlers, (d) => d['method']), 0.5)).toBeTruthy();
+    expect(deepEqual(match({ method: '/', x: 1, y: 2 }, handlers, (d) => d['methodd']), undefined)).toBeTruthy();
+  });
+
+  it("redux/reducer", () => {
+    interface Action {
+      type: '+' | '-' | '*' | '/';
+      payload: {
+        x: number;
+        y: number;
+      };
+    }
+
+    const handlers = {
+      '+': (action: Action) => action.payload.x + action.payload.y,
+      '-': (action: Action) => action.payload.x - action.payload.y,
+      '*': (action: Action) => action.payload.x * action.payload.y,
+      '/': (action: Action) => action.payload.x / action.payload.y,
+    };
+
+    expect(deepEqual(match({ type: '+', payload: { x: 1, y: 2 } }, handlers, (d) => d['type']), 3)).toBeTruthy();
+    expect(deepEqual(match({ type: '-', payload: { x: 1, y: 2 } }, handlers, (d) => d['type']), -1)).toBeTruthy();
+    expect(deepEqual(match({ type: '*', payload: { x: 1, y: 2 } }, handlers, (d) => d['type']), 2)).toBeTruthy();
+    expect(deepEqual(match({ type: '/', payload: { x: 1, y: 2 } }, handlers, (d) => d['type']), 0.5)).toBeTruthy();
+    expect(deepEqual(match({ type: '/', payload: { x: 1, y: 2 } }, handlers, (d) => d['typed']), undefined)).toBeTruthy();
+  });
+});
---
+++ packages/match/src/match.ts
@@ -0,0 +1,12 @@
+
+/**
+ * 
+ * @param data object
+ * @param handlers reducers
+ * @param keyFn key function
+ */
+export function match<T, R>(data: T, handlers: Record<string, (d: T) => R>, keyFn: (data: T) => string): R | undefined {
+  const key = keyFn(data);
+
+  return handlers[key] ? handlers[key].call(null, data) : undefined;
+}
