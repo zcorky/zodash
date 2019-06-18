f07317d49170cae5fdbdfb758a16ba2333c1e2a4
whatwewant <tobewhatwewant@gmail.com>
2019-06-18 13:19:13 +0800

    :sparkles:(packages/filter): add filter package

---
+++ packages/filter/README.md
@@ -0,0 +1,11 @@
+# `@zodash/reduce`
+
+> TODO: description
+
+## Usage
+
+```
+const reduce = require('@zodash/reduce');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/filter/__tests__/filter.test.ts
@@ -0,0 +1,15 @@
+"use strict";
+
+import { deepEqual } from "@zcorky/deep-equal";
+import { filter } from "../src/filter";
+
+describe("@zodash/filter", () => {
+  it("same as builtin filter", () => {
+    const v = [{ x: 1, y: 2 }, { x: 3, y: 4 }];
+
+    const v1 = filter(v, (c, i) => c.x > 1);
+    const v2 = v.filter((c, i) => c.x > 1);
+
+    expect(deepEqual(v1, v2)).toBeTruthy();
+  });
+});
