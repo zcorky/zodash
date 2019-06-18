b3ea69363e3ef02ab085a9615c9c0c3ed02773c5
whatwewant <tobewhatwewant@gmail.com>
2019-06-18 18:33:59 +0800

    :sparkles:(packages/zipObject): realize zipObject

---
+++ packages/zipObject/src/zipObject.ts
@@ -0,0 +1,9 @@
+
+/**
+ * Creates an array of grouped elements, the first of which contains the first elements of the given arrays, the second of which contains the second elements of the given arrays, and so on.
+ * 
+ * @param args any[][]
+ */
+export function zipObject(keys: string[], values: any[]) {
+  return keys.reduce((all, key, index) => (all[key] = values[index], all), {});
+}
