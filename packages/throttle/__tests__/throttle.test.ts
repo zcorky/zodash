2c4b06ab78e687282e24d1324738a200de9cbaaa
whatwewant <tobewhatwewant@gmail.com>
2019-06-18 18:34:58 +0800

    :sparkles:(packages/throttle): realize throttle

---
+++ packages/throttle/__tests__/throttle.test.ts
@@ -0,0 +1,9 @@
+"use strict";
+
+import { deepEqual } from "@zcorky/deep-equal";
+import { throttle } from "../src/throttle";
+
+describe("@zodash/throttle", () => {
+  it("common", () => {
+  });
+});
---
+++ packages/throttle/src/throttle.ts
@@ -0,0 +1,1 @@
+export { default as throttle } from '@zcorky/throttle';
