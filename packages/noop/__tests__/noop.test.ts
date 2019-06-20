f3de4078b342ad7c1ddb90fdbedcc3b808617667
whatwewant <tobewhatwewant@gmail.com>
2019-06-21 03:49:27 +0800

    :white_check_mark:(packages/noop): fix test

--- packages/noop/__tests__/noop.test.ts
+++ packages/noop/__tests__/noop.test.ts
@@ -2,7 +2,7 @@ import { noop } from "../src/noop";
 
 describe("@zodash/noop", () => {
   it("needs tests", () => {
-    expect(noop()).toBeNull();
+    expect(noop()).toBeUndefined();
   });
 
   it("typescript generic", () => {
