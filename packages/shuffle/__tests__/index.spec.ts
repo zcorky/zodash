43a961c22c7f6a9291e375eb8f4adbe2a0ab3573
whatwewant <tobewhatwewant@gmail.com>
2019-09-21 14:53:26 +0800

    :sparkles:(@zodash/shuffle): add shuffle package

---
+++ packages/shuffle/__tests__/index.spec.ts
@@ -0,0 +1,51 @@
+import { shuffle } from '../src/shuffle';
+
+describe("@zodash/shuffle", () => {
+  it('shuffle number[]', () => {
+    const array = [1, 2, 3, 4, 5];
+    
+    for (let i = 0; i < 10; ++i) {
+      const shuffledArray = shuffle(array);
+      // console.log(array, shuffledArray);
+
+      // length
+      expect(shuffledArray.length).toEqual(array.length);
+
+      const _array = new Array(array.length);
+      shuffledArray.forEach(value => {
+        // value
+        expect(array.includes(value)).toBeTruthy();
+        // index
+        const index = array.indexOf(value);
+        _array[index] = value;
+      });
+
+      // value all used
+      expect(_array.every(v => !!v)).toBeTruthy();
+    }
+  });
+
+  it('shuffle string[]', () => {
+    const array = ['zero', 'any', 'eason', 'cole', 'me'];
+    
+    for (let i = 0; i < 10; ++i) {
+      const shuffledArray = shuffle(array);
+      // console.log(array, shuffledArray);
+      
+      // length
+      expect(shuffledArray.length).toEqual(array.length);
+
+      const _array = new Array(array.length);
+      shuffledArray.forEach(value => {
+        // value
+        expect(array.includes(value)).toBeTruthy();
+        // index
+        const index = array.indexOf(value);
+        _array[index] = value;
+      });
+
+      // value all used
+      expect(_array.every(v => !!v)).toBeTruthy();
+    }
+  });
+});
---
+++ packages/shuffle/src/shuffle.ts
@@ -0,0 +1,37 @@
+const nativeFloor = Math.floor;
+const nativeCeil = Math.ceil;
+const nativeRandom = Math.random;
+
+function copyArray<T>(array: T[]) {
+  return [...array];
+}
+
+function baseRandom(lower: number, upper: number) {
+  return lower + nativeFloor(nativeRandom() * (upper - lower + 1))
+}
+
+function swap(array: any[], left: number, right: number) {
+  const value = array[left];
+  array[left] = array[right];
+  array[right] = value;
+}
+
+/**
+ * Creates an array of shuffled values
+ * 
+ * @param array the collection to shufflee
+ * @returns shuffled collection
+ */
+export function shuffle<T>(collection: T[]) {
+  const _collection = copyArray(collection);
+  let index = -1;
+  let length = _collection.length;
+  let lastIndex = length - 1;
+
+  while (++index < length) {
+    const rand = baseRandom(index, lastIndex);
+    swap(_collection, index, rand);
+  }
+
+  return _collection;
+}
