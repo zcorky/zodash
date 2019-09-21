c6e34ff48a935b7dd6bfdb846945604528aa33b2
whatwewant <tobewhatwewant@gmail.com>
2019-09-21 15:30:41 +0800

    :sparkles:(@zodash/unique): add unique package

---
+++ packages/unique/__tests__/index.spec.ts
@@ -0,0 +1,67 @@
+import { unique } from '../src/unique';
+
+describe("@zodash/unique", () => {
+  it('unique number[]', () => {
+    const array = [1, 2, 2, 2, 3];
+    
+    const uniquedArray = unique(array);
+    expect(uniquedArray).toEqual([1, 2, 3]);
+  });
+
+  it('unique string[]', () => {
+    const array = ['zero', 'any', 'zero', 'zero', 'zero'];
+    
+    const uniquedArray = unique(array);
+    expect(uniquedArray).toEqual(['zero', 'any']);
+  });
+
+  it('unique object[] with by key', () => {
+    const users = [{
+      id: '0x10',
+      nickname: 'Zero',
+    }, {
+      id: '0x11',
+      nickname: 'Any',
+    }, {
+      id: '0x10',
+      nickname: 'Zero',
+    }, {
+      id: '0x11',
+      nickname: 'Any',
+    }];
+    
+    const uniquedArray = unique(users, user => user.id);
+    expect(uniquedArray).toEqual([{
+      id: '0x10',
+      nickname: 'Zero',
+    }, {
+      id: '0x11',
+      nickname: 'Any',
+    }]);
+  });
+
+  it('unique object[] with by key, next value will overwrite prev value', () => {
+    const users = [{
+      id: '0x10',
+      nickname: 'zero',
+    }, {
+      id: '0x11',
+      nickname: 'any',
+    }, {
+      id: '0x10',
+      nickname: 'Zero',
+    }, {
+      id: '0x11',
+      nickname: 'Any',
+    }];
+    
+    const uniquedArray = unique(users, user => user.id);
+    expect(uniquedArray).toEqual([{
+      id: '0x10',
+      nickname: 'Zero',
+    }, {
+      id: '0x11',
+      nickname: 'Any',
+    }]);
+  });
+});
---
+++ packages/unique/src/unique.ts
@@ -0,0 +1,10 @@
+export function unique<T>(array: T[], by: (value: T) => string): T[]
+export function unique<T>(array: T[]): T[]
+export function unique<T>(array?: T[], by?: (value: T) => string) {
+  return Object
+    .values(array.reduce((all, value) => {
+      const key = typeof by === 'function' ? by(value) : value as any as string; 
+      all[key] = value;
+      return all;
+    }, {} as { [key: string]: T }));
+}
