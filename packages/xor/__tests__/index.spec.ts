3fba72a72fdf43028f07f99dc1f1ccadc20df6ad
whatwewant <tobewhatwewant@gmail.com>
2019-09-21 17:03:52 +0800

    :sparkles:(@zodash): add xor package

---
+++ packages/xor/__tests__/index.spec.ts
@@ -0,0 +1,49 @@
+import { xor } from '../src/xor';
+
+describe("@zodash/xor", () => {
+  it('xor number[]', () => {
+    expect(xor([3, 2, 1], [4, 2])).toEqual([1, 3, 4]);
+  });
+
+  it('xor string[]', () => {
+    expect(xor(['zero', 'any', 'zero', 'zero', 'zero'], ['any'], ['eason', 'cole', 'any'])).toEqual(['zero', 'eason', 'cole']);
+  });
+
+  it('xor object[] with by key', () => {
+    const oa = [{
+      id: '0x10',
+      nickname: 'Zero',
+    }, {
+      id: '0x11',
+      nickname: 'Any',
+    }, {
+      id: '0x10',
+      nickname: 'Zero',
+    }, {
+      id: '0x11',
+      nickname: 'Any',
+    }];
+
+    const ob = [{
+      id: '0x11',
+      nickname: 'Any',
+    }];
+
+    const oc = [{
+      id: '0x12',
+      nickname: 'Eason',
+    }, {
+      id: '0x11',
+      nickname: 'Any',
+    }];
+    
+    const newUsers = xor(user => user.id, oa, ob, oc);
+    expect(newUsers).toEqual([{
+      id: '0x10',
+      nickname: 'Zero',
+    }, {
+      id: '0x12',
+      nickname: 'Eason',
+    }]);
+  });
+});
---
+++ packages/xor/src/xor.ts
@@ -0,0 +1,38 @@
+/**
+ * Creates an array of unique values.
+ * 
+ * @param by (optional)
+ * @param arrays array of array
+ */
+export function xor<T>(by: (value: T) => string, ...arrays: T[][]): T[]
+export function xor<T>(...arrays: T[][]): T[]
+export function xor<T>(by: any, ...arrays: T[][]) {
+  const isFn = typeof by === 'function';
+  const _by = isFn ? by : undefined;
+  const _arrays = isFn ? arrays : [by, ...arrays] as T[][];
+
+  const arrayMap = {};
+  
+  const mapArray = _arrays.map(array => {
+    const _map = array.reduce((all, value) => {
+      const key = isFn ? _by(value) : value as any as string; 
+      all[key] = 1;
+      arrayMap[key] = value;
+      return all;
+    }, {} as { [key: string]: number });
+    return _map;
+  });
+
+  const mapCount = mapArray.reduce((all, map) => {
+    Object.keys(map).forEach(key => {
+      all[key] = !all[key] ? 1 : all[key] + 1;
+    });
+    return all;
+  }, {} as { [key: string]: number });
+
+  return Object
+    .keys(mapCount).filter(key => {
+      return mapCount[key] === 1;
+    })
+    .map(key => arrayMap[key]);
+}
