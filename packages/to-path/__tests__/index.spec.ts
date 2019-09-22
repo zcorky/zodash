820d290285991e40f930c39562021aee5a36a8cf
whatwewant <tobewhatwewant@gmail.com>
2019-09-22 17:41:17 +0800

    :sparkles:(@zodash): add to-path package

---
+++ packages/to-path/__tests__/index.spec.ts
@@ -0,0 +1,19 @@
+import { toPath } from '../src/to-path';
+
+describe("@zodash/unique", () => {
+  it('a.b.c => [a, b, c]', () => {
+    expect(toPath('a.b.c')).toEqual(['a', 'b', 'c']);
+  });
+
+  it('a[0].b.c => [a, 0, b, c]', () => {
+    expect(toPath('a[0].b.c')).toEqual(['a', '0', 'b', 'c']);
+  });
+
+  it('a.0.b.c => [a, 0, b, c]', () => {
+    expect(toPath('a.0.b.c')).toEqual(['a', '0', 'b', 'c']);
+  });
+
+  it('dynamic data empty string', () => {
+    expect(toPath(null)).toEqual([]);
+  });
+});
