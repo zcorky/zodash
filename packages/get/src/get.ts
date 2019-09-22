6a80ea4777a3f54d27f926ada82c725d00b971ad
whatwewant <tobewhatwewant@gmail.com>
2019-09-22 17:52:01 +0800

    :sparkles:(@zodash/get): use @zodash/to-path

--- packages/get/src/get.ts
+++ packages/get/src/get.ts
@@ -1,3 +1,5 @@
+import { toPath } from '@zodash/to-path';
+
 /**
  * Get the value at path of object.
  * If the resolved values is undefined, the defaultValue is returnted in its place.
@@ -7,9 +9,7 @@
  * @param defaultValue The value returned for undefined resolved values.
  */
 export function get<T extends object, D>(value: T, path: string, defaultValue: D = undefined): D {
-  return path
-    .replace(/\[(\w+)\]/g, '.$1')
-    .split('.')
+  return toPath(path)
     .reduce((p, k) => {
       if (p && typeof p[k] !== 'undefined') {
         return p[k];
