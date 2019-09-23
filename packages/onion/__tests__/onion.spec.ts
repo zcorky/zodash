5774bea2ce81b221d4eca97281d4298610006aa1
whatwewant <tobewhatwewant@gmail.com>
2019-09-23 08:09:56 +0800

    :sparkles:(@zodash/onion): reafactor with request => response or error

--- packages/onion/__tests__/onion.spec.ts
+++ packages/onion/__tests__/onion.spec.ts
@@ -1,41 +1,64 @@
-import { Onion } from '../src/onion';
+import { Onion, Context } from '../src/onion';
 
 // compose vs pipe
 
+declare module '../src/onion' {
+  interface Request {
+    state: number[];
+  }
+  interface Response {
+    state: number[];
+  }
+
+  interface Context {
+    state: number[];
+  }
+}
+
 describe("@zodash/onion", () => {
   it('works', () => {
-    interface Context {
-      state: any[];
-    }
+    // interface Context {
+    //   state: any[];
+    // }
     
     const app = new Onion();
 
-    const m1 = async (context: Context, next: Function) => {
-      context.state.push(1);
+    const m = async (ctx: Context, next: Function) => {
+      ctx.state = ctx.response.state = ctx.request.state;
       await next();
-      context.state.push(6);
+    }
+
+    const m1 = async (ctx: Context, next: Function) => {
+      ctx.state.push(1);
+      await next();
+      ctx.state.push(6);
     };
 
-    const m2 = async (context: Context, next: Function) => {
-      context.state.push(2);
+    const m2 = async (ctx: Context, next: Function) => {
+      ctx.state.push(2);
       await next();
-      context.state.push(5);
+      ctx.state.push(5);
     };
 
-    const m3 = async (context: Context, next: Function) => {
-      context.state.push(3);
+    const m3 = async (ctx: Context, next: Function) => {
+      ctx.state.push(3);
       await next();
-      context.state.push(4);
+      ctx.state.push(4);
     };
 
+    app.use(m)
     app.use(m1);
     app.use(m2);
     app.use(m3);
 
-    app.start();
+    // app.start();
 
-    app.execute({ state: [] }).then((context) => {
-      expect((context as any).state).toEqual([1, 2, 3, 4, 5, 6]);
+    app.request({ state: [] }).then((response) => {
+      expect(response.state).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+
+    app.request({ state: [] }).then((response) => {
+      expect(response.state).toEqual([1, 2, 3, 4, 5, 6]);
     });
   });
 });
--- packages/onion/src/onion.ts
+++ packages/onion/src/onion.ts
@@ -1,30 +1,68 @@
 import { compose, Middleware } from '@zodash/compose';
 
 export interface IOnion {
-  use(middleware: Middleware<Context>): void;
-  start(): void;
-  execute<R>(context: Context): Promise<R>;
+  use(middleware: Middleware<Context>): ThisType<any>;
+  callback(): (req: Request, res: Response) => Promise<void>;
+  request(request: Request): Promise<Response>;
 }
 
 export interface Context {
+  options: Options;
+  request: Request;
+  response: Response;
+}
+
+export interface Request {
 
 }
 
-export class Onion {
+export interface Response {
+
+}
+
+export interface Options {
+
+}
+
+export class Onion implements IOnion {
   private middlewares: Middleware<Context>[] = [];
-  private channel: Middleware<Context>;
+  private handler: Middleware<Context>;
+  private _callback: (request: Request, response: Response) => Promise<any>;
+
+  constructor(private readonly options: Options = {} as Options) {}
 
   public use(middleware: Middleware<Context>) {
     this.middlewares.push(middleware);
+    return this;
   }
 
-  public start() {
-    const fn = compose(...this.middlewares);
-    this.channel = fn;
+  public callback() {
+    if (!this.handler) {
+      const fn = compose(...this.middlewares);
+      this.handler = fn;
+    }
+
+    return (request: Request, response: Response) => {
+      const context = this.createContext(request, response);
+      return this.handler(context);
+    };
   }
 
-  public async execute(context: Context) {
-    await this.channel(context);
-    return context;
+  public async request(request: Request): Promise<Response> {
+    if (!this._callback) {
+      this._callback = this.callback();
+    }
+
+    const response = {} as Response;
+    await this._callback(request, response);
+    return response;
+  }
+
+  private createContext(request: Request, response: Response): Context {
+    return {
+      options: this.options,
+      request,
+      response,
+    };
   }
 }
