d1e1951461eb2f85e53958155fe04865d34ae097
whatwewant <tobewhatwewant@gmail.com>
2019-11-29 23:40:34 +0800

    :sparkles:(@zodash/url-join): add simple url join

---
+++ packages/url-join/CHANGELOG.md
@@ -0,0 +1,40 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+## [0.0.4](https://github.com/zcorky/zodash/compare/@zodash/unfetch@0.0.3...@zodash/unfetch@0.0.4) (2019-10-30)
+
+**Note:** Version bump only for package @zodash/unfetch
+
+
+
+
+
+## 0.0.3 (2019-10-30)
+
+**Note:** Version bump only for package @zodash/unfetch
+
+
+
+
+
+## [0.0.2](https://github.com/zcorky/zodash/compare/@zodash/unfetch@0.0.1...@zodash/unfetch@0.0.2) (2019-09-22)
+
+**Note:** Version bump only for package @zodash/unfetch
+
+
+
+
+
+## 0.0.1 (2019-09-22)
+
+**Note:** Version bump only for package @zodash/unfetch
+
+
+
+
+
+## 0.0.1 (2019-09-21)
+
+**Note:** Version bump only for package @zodash/unique
---
+++ packages/url-join/__tests__/index.spec.ts
@@ -0,0 +1,23 @@
+import { urlJoin } from '../src/url-join';
+
+describe("@zodash/unfetch", () => {
+  it('should work for simple case', () => {
+		expect(urlJoin('http://www.google.com/', 'foo/bar', '?test=123'))
+      .toBe('http://www.google.com/foo/bar?test=123');
+	});
+
+	it('should be able to join protocol', function () {
+    expect(urlJoin('http:', 'www.google.com/', 'foo/bar', '?test=123'))
+      .toBe('http://www.google.com/foo/bar?test=123');
+	});
+	
+	it('should be able to join protocol with slashes', function () {
+    expect(urlJoin('http://', 'www.google.com/', 'foo/bar', '?test=123'))
+      .toBe('http://www.google.com/foo/bar?test=123');
+  });
+
+  it('should remove extra slashes', function () {
+    expect(urlJoin('http:', 'www.google.com///', 'foo/bar', '?test=123'))
+      .toBe('http://www.google.com/foo/bar?test=123');
+  });
+});
