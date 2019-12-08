7e088f806cfb0537e3b2642f591b3115d78a78a0
whatwewant <tobewhatwewant@gmail.com>
2019-12-08 18:35:02 +0800

    :sparkles:(@zodash/strategy): fix returned value type instead of any

--- packages/strategy/src/strategy.ts
+++ packages/strategy/src/strategy.ts
@@ -7,7 +7,7 @@
  * @param keyFn the strategy make function
  */
 export function strategy<T, R>(strategies: Record<string, (d: T) => R>, strategyKey: (data: T) => string) {
-  return (data: T) => {
+  return (data: T): R => {
     const key = strategyKey(data);
     return strategies[key] ? strategies[key].call(null, data) : undefined;
   }
