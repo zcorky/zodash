f605505aea80cabdf888cddf7e5572282b3d5e7f
whatwewant <tobewhatwewant@gmail.com>
2019-12-24 12:49:08 +0800

    :sparkles:(@zodash/get): refactor, support [] usage

--- packages/get/src/get.ts
+++ packages/get/src/get.ts
@@ -8,13 +8,47 @@ import { toPath } from '@zodash/to-path';
  * @param path The path of the property to get.
  * @param defaultValue The value returned for undefined resolved values.
  */
-export function get<T extends object, D>(value: T, path: string, defaultValue: D = undefined): D {
-  return toPath(path)
-    .reduce((p, k) => {
-      if (p && typeof p[k] !== 'undefined') {
-        return p[k];
-      }
+export function get<T extends object, D = any>(value: T, path: string, defaultValue: D = undefined): D {
+  // return toPath(path)
+  //   .reduce((p, k) => {
+  //     if (p && typeof p[k] !== 'undefined') {
+  //       return p[k];
+  //     }
 
-      return defaultValue;
-    }, value);
+  //     return defaultValue;
+  //   }, value);
+  return getValue(value, toPath(path)) || defaultValue;
+}
+
+export function getValue(parent: object | object[], paths: string[], currentIndex: number = 0) {
+  if (typeof parent === 'undefined') return undefined;
+
+  const token = paths[currentIndex];
+  const prevToken = paths[currentIndex - 1];
+  const nextToken = paths[currentIndex + 1];
+
+  // object
+  // console.log(prevToken, token, nextToken);
+  // console.log(parent);
+  // console.log(paths);
+  // console.log('####');
+  // console.log(parent, typeof parent);
+  if (token !== '[]') {
+    if (typeof nextToken === 'undefined') {
+      return parent[token];
+    }
+
+    return getValue(parent[token], paths, currentIndex + 1);
+  }
+  
+  // array
+  if (!Array.isArray(parent)) {
+    return undefined;
+  }
+
+  if (typeof nextToken === 'undefined') {
+    return parent;
+  }
+
+  return (parent as any).map((v: any) => getValue(v, paths, currentIndex + 1));
 }
