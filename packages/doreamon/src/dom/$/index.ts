d5cf2b1997f6a18f27f6663e92413db3e5e63099
whatwewant <tobewhatwewant@gmail.com>
2020-09-20 10:05:31 +0800

    chore(doreamon): dom.$.{onScrollToTop,onScrollToBottom,scrollToTop,scrollToBottom}

--- packages/doreamon/src/dom/$/index.ts
+++ packages/doreamon/src/dom/$/index.ts
@@ -1,10 +1,24 @@
+import { debounce } from '@zodash/debounce';
+
 export type Selector = string;
+
+export type El = Element | Selector;
+
+export type Listener = <T extends Event>(e?: T) => void;
+
+export type Unsubscibe = () => void;
 
 export type $S = (selector: Selector | Element) => Element;
 
 export interface DOMUtils extends $S {
-  on($el: Element | Selector, eventName: string, listener: <T extends Event>(e: T) => void): void;
-  off($el: Element | Selector, eventName: string, listener: <T extends Event>(e: T) => void): void;
+  on($el: El, eventName: string, listener: Listener): void;
+  off($el: El, eventName: string, listener: Listener): void;
+  //
+  onScrollToBottom($el: El, listener: Listener, threshold?: number): Unsubscibe;
+  onScrollToTop($el: El, listener: Listener, threshold?: number): Unsubscibe;
+  //
+  scrollToTop($el: El, animated?: boolean): void;
+  scrollToBottom($el: El, animated?: boolean): void;
 }
 
 const $: DOMUtils = (selector) => {
@@ -21,6 +35,70 @@ $.on = ($el, eventName, cb) => {
 
 $.off = ($el, eventName, cb) => {
   $($el).removeEventListener(eventName, cb);
+}
+
+$.onScrollToTop = ($element, cb, threshold = 50) => {
+  const $el = $($element);
+  
+  const callback = debounce((e) => {
+    const scrollTop = $el?.scrollTop;
+
+    if (scrollTop < threshold) {
+      cb(e);
+    }
+  }, 250);
+
+  $el.addEventListener('scroll', callback);
+  
+  return () => {
+    if ($el) return;
+    
+    $el.removeEventListener('scroll', callback);
+  };
+}
+
+$.onScrollToBottom = ($element, cb, threshold = 50) => {
+  const $el = $($element);
+
+  const callback = debounce((e) => {
+    const scrollTop = $el?.scrollTop;
+    const scrollHeight = $el?.scrollHeight;
+
+    if (scrollTop + threshold >= scrollHeight) {
+      cb(e);
+    }
+  }, 250);
+
+  $el.addEventListener('scroll', callback);
+
+  return () => {
+    if ($el) return;
+    
+    $el.removeEventListener('scroll', callback);
+  };
+}
+
+$.scrollToTop = ($element, animated) => {
+  const $el = $($element);
+  
+  setTimeout(() => {
+    $el && $el.scrollTo({
+      top: 0,
+      behavior: animated ? 'smooth' : undefined,
+    });
+  }, 0);
+}
+
+$.scrollToBottom = ($element, animated) => {
+  const $el = $($element);
+  const scrollHeight = $el?.scrollHeight;
+  
+  setTimeout(() => {
+    $el && $el.scrollTo({
+      top: scrollHeight,
+      behavior: animated ? 'smooth' : undefined,
+    });
+  }, 0);
 }
 
 export default $;
