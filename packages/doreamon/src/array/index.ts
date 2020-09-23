6c9a2550935178d7f9a44dd9712a62f781522cba
whatwewant <tobewhatwewant@gmail.com>
2020-09-23 11:22:17 +0800

    chore(doremaon): add array.index

--- packages/doreamon/src/array/index.ts
+++ packages/doreamon/src/array/index.ts
@@ -1,9 +1,11 @@
 import unique from './unique';
 import find from './find';
 import groupBy from './group-by';
+import index from './index';
 
 export default {
   unique,
   find,
   groupBy,
+  index,
 };
