5a75d40f33f9e2a0f8337c8145db8b61cadfc5a1
whatwewant <tobewhatwewant@gmail.com>
2020-11-12 16:21:04 +0800

    chore(debug): add @zodash/debug

---
+++ packages/debug/src/index.ts
@@ -0,0 +1,13 @@
+import { Logger } from '@zodash/logger';
+
+const logger = new Logger('COMMON');
+
+export function debug(message: string, ...args: any[]) {
+  if (process.env.NODE_ENV === 'production') {
+    return ;
+  }
+
+  logger.debug(message, ...args);
+}
+
+export default debug;
