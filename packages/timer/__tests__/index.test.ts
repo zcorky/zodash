00fe1f786ec1f96d2cce07f6bd598e7421ffe9d6
whatwewant <tobewhatwewant@gmail.com>
2020-11-12 11:01:15 +0800

    chore(timer): add @zodash/timer

---
+++ packages/timer/__tests__/index.test.ts
@@ -0,0 +1,7 @@
+import timer from '../src';
+
+describe('@zodash/timer', () => {
+  it('run', async () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ packages/timer/src/index.ts
@@ -0,0 +1,48 @@
+import { Event as Emitter } from '@zodash/event';
+import moment, { Moment } from '@zcorky/moment';
+
+export interface IOptions {
+  interval?: number;
+  deadlineAt?: Date;
+}
+
+export class Timer extends Emitter {
+  private it: NodeJS.Timeout;
+  private interval = this.options?.interval || 1000;
+
+  public static create(options?: IOptions) {
+    return new Timer(options);
+  }
+
+  constructor(private readonly options?: IOptions) {
+    super();
+  }
+
+  public onUpdate(cb: <T extends Moment | number>(momentOrDiff: T) => void) {
+    this.on('update', cb);
+  }
+
+  public start() {
+    this.it = setTimeout(() => {
+      const time = moment();
+      if (this.options.deadlineAt) {
+        const diffTime = time.diff(this.options.deadlineAt);
+
+        this.emit('update', diffTime);
+      } else {
+        this.emit('update', time);
+      }
+
+      this.start();
+    }, this.interval);
+  }
+
+  public stop() {
+    if (this.it) {
+      clearTimeout(this.it);
+      this.it = null;
+    }
+  }
+}
+
+export default Timer;
