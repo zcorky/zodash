161f17f59fe7f46acce15d0869502c161f96fea3
whatwewant <tobewhatwewant@gmail.com>
2020-12-09 16:51:38 +0800

    chore(retry): add @zodash/retry

---
+++ packages/retry/__tests__/index.test.ts
@@ -0,0 +1,100 @@
+import retry from '../src';
+
+describe('@zodash/Container', () => {
+  it('works', async () => {
+    let i = 0;
+
+    const res = await retry(async () => {
+      i++;
+
+      if (i !== 4) {
+        throw new Error();
+      }
+
+      return i;
+    }, { times: 3 });
+
+    expect(i).toBe(4);
+    expect(res).toBe(4);
+  });
+
+  it('default times 0', async () => {
+    let i = 0;
+
+    const res = await retry(async () => {
+      i++;
+      return i;
+    });
+
+    expect(i).toBe(1);
+    expect(res).toBe(1);
+  });
+
+  it('throw error', async () => {
+    let i = 0;
+
+    const fn = async () => {
+      i++;
+
+      // console.log('xxx:', i);
+      if (i <= 4) {
+        throw new Error('throw error');
+      }
+    };
+
+    i = 0;
+    await expect(retry(fn)) // 4
+      .rejects
+      .toThrowError();
+
+    i = 0;
+    await expect(retry(fn, { times: 1 })) // 4
+      .rejects
+      .toThrowError();
+
+    i = 0;
+    await expect(retry(fn, { times: 2 })) // 4
+      .rejects
+      .toThrowError();
+
+    i = 0;
+    await expect(retry(fn, { times: 3 })) // 4
+      .rejects
+      .toThrowError();
+
+    i = 0;
+    await expect(retry(fn, { times: 4 })) // 4
+      .resolves
+      .not
+      .toThrowError();
+
+    i = 0;
+    await expect(retry(fn, { times: 5 })) // 4
+      .resolves
+      .not
+      .toThrowError();
+  });
+
+  it('delay', async () => {
+    let i = 0;
+
+    const fn = async () => {
+      i++;
+
+      // console.log('xxx:', i);
+      if (i <= 2) {
+        throw new Error('throw error');
+      }
+    };
+
+    i = 0;
+    let startedAt = Date.now();
+    await retry(fn, { times: 2 });
+    expect(Date.now() - startedAt).toBeLessThan(100);
+
+    i = 0;
+    startedAt = Date.now();
+    await retry(fn, { times: 2, delay: 100 });
+    expect(Date.now() - startedAt).toBeGreaterThan(100);
+  });
+});
---
+++ packages/retry/src/index.ts
@@ -0,0 +1,35 @@
+import { delay } from '@zodash/delay';
+
+export interface IOptions {
+  /**
+   * Retry Max Times
+   */
+  times: number;
+
+  /**
+   * Delay Ms (MiliSeconds)
+   */
+  delay?: number;
+}
+
+export async function retry<R extends any>(fn: () => Promise<R>, options?: IOptions) {
+  const times = options?.times ?? 0;
+  const delayMs = options?.delay ?? 0; 
+  
+  for (let i = times; i >= 0; i--) {
+    try {
+      return await fn();
+    } catch (error) {
+      if (i === 0) {
+        throw error;
+      }
+
+      // retry after
+      if (delayMs) {
+        await delay(delayMs);
+      }
+    }
+  }
+}
+
+export default retry;
