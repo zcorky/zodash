dc610576ba7180f4d318e6f17b3a01c54aa12ff8
whatwewant <tobewhatwewant@gmail.com>
2020-12-17 13:12:32 +0800

    chore(storage): add @zodash/storage

---
+++ packages/storage/__tests__/index.test.ts
@@ -0,0 +1,7 @@
+import Storage from '../src';
+
+describe('@zodash/storage', () => {
+  it('works', async () => {
+    expect(true).toBeTruthy();
+  });
+});
---
+++ packages/storage/src/index.ts
@@ -0,0 +1,8 @@
+import Storage, { IStorage } from '@zcorky/storage';
+
+export {
+  Storage,
+  IStorage,
+};
+
+export default Storage;
