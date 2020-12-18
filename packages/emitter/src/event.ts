ef8eb3f62993fc45856d708e88f5a9b4f6323318
whatwewant <tobewhatwewant@gmail.com>
2020-12-18 19:55:12 +0800

    chore(event): update event emitter, support typed events

--- packages/emitter/src/event.ts
+++ packages/emitter/src/event.ts
@@ -1,10 +1,10 @@
 import { IEvent, Event } from '@zodash/event';
 
-export interface IEmitter extends IEvent {
+export interface IEmitter<Events = any> extends IEvent<Events> {
 
 }
 
-export class Emitter extends Event implements IEmitter {
+export class Emitter<Events = any> extends Event<Events> implements IEmitter<Events> {
   
 }
 
--- packages/event/src/event.ts
+++ packages/event/src/event.ts
@@ -1,28 +1,32 @@
 import { once } from '@zodash/once';
 
-export type Listener = (...args: any[]) => void;
+export type Arguments<T> = [T] extends [(...args: infer U) => any]
+  ? U
+  : [T] extends [void] ? [] : [T]
 
-export interface IEvent {
-  on(event: string, listener: Listener): this;
-  off(event: string, listener: Listener): this;
-  addListener(event: string, listener: Listener): this;
-  removeListener(event: string, listener: Listener): this;
-  removeAllListeners(event: string): this;
-  emit(event: string, ...args: any[]): boolean;
-  once(event: string, listener: Listener): this;
-  listeners: Record<string, Listener[]>;
+export type Listener = any; // T extends any ? (...args: any[]) => void : T;
+
+export interface IEvent<Events> {
+  on<E extends keyof Events>(event: E, listener: Events[E]): this;
+  off<E extends keyof Events>(event: E, listener: Events[E]): this;
+  addListener<E extends keyof Events>(event: E, listener: Events[E]): this;
+  removeListener<E extends keyof Events>(event: E, listener: Events[E]): this;
+  removeAllListeners<E extends keyof Events>(event: E): this;
+  emit<E extends keyof Events>(event: E, ...args: Arguments<Events[E]>): boolean;
+  once<E extends keyof Events>(event: E, listener: Events[E]): this;
+  listeners: Record<keyof Events, Listener[]>;
 }
 
 const LISTENERS = Symbol('listeners');
 
-export class Event implements IEvent {
-  private [LISTENERS]: Record<string, Listener[]> = {};
+export class Event<Events = any> implements IEvent<Events> {
+  private [LISTENERS]: Record<keyof Events, Listener[]> = {} as any;
 
   public get listeners() {
     return this[LISTENERS];
   }
 
-  public on(event: string, listener: Listener) {
+  public on<E extends keyof Events>(event: E, listener: Events[E]) {
     if (!this.listeners[event]) {
       this.listeners[event] = [];
     }
@@ -31,7 +35,7 @@ export class Event implements IEvent {
     return this;
   }
 
-  public off(event: string, listener: Listener) {
+  public off<E extends keyof Events>(event: E, listener: Events[E]) {
     if (this.listeners[event]) {
       const all = this.listeners[event];
       all.splice(all.indexOf(listener), 1);
@@ -40,16 +44,16 @@ export class Event implements IEvent {
     return this;
   }
 
-  public once(event: string, listener: Listener) {
+  public once<E extends keyof Events>(event: E, listener: Events[E]) {
     if (!this.listeners[event]) {
       this.listeners[event] = [];
     }
 
-    this.listeners[event].push(once(listener));
+    this.listeners[event].push(once(listener as any));
     return this;
   }
 
-  public emit(event: string, ...args: any[]) {
+  public emit<E extends keyof Events>(event: E, ...args: Arguments<Events[E]>) {
     if (this.listeners[event]) {
       const partListeners = this.listeners[event];
       partListeners.forEach(listener => {
@@ -60,15 +64,15 @@ export class Event implements IEvent {
     return false;
   }
 
-  public addListener(event: string, listener: Listener) {
+  public addListener<E extends keyof Events>(event: E, listener: Events[E]) {
     return this.on(event, listener);
   }
 
-  public removeListener(event: string, listener: Listener) {
+  public removeListener<E extends keyof Events>(event: E, listener: Events[E]) {
     return this.off(event, listener);
   }
 
-  public removeAllListeners(event: string) {
+  public removeAllListeners<E extends keyof Events>(event: E) {
     if (this.listeners[event]) {
       delete this.listeners[event];
     }
