e36c07c51f114f624bb1283a7b87acfb924df05b
whatwewant <tobewhatwewant@gmail.com>
2020-12-22 13:03:42 +0800

    chore(worker): improve data structure and add docs

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -6,20 +6,28 @@
 export type IWorkerFn<D extends any[], R> = (...args: D) => Promise<R>;
 
 export interface IWorker<D extends any[], R> {
+  compile(): this;
+  watch(): this;
   run: IWorkerFn<D, R>;
+  stop(): Promise<void>;
 }
 
-export class WeWorker<D extends any[], R> implements IWorker<D, R> {
+export class WebWorker<D extends any[], R> implements IWorker<D, R> {
   private worker: Worker;
   private id = createIdGenerator();
   private tasks: Record<string, any> = {};
 
+  /**
+   * Create a function as a worker
+   * 
+   * @param fn raw function
+   */
   public static create<D extends any[], R>(fn: IWorkerFn<D, R>) {
-    const worker = new WeWorker(fn);
+    const worker = new WebWorker(fn);
 
-    worker.compile();
-
-    worker.watch();
+    worker
+      .compile()
+      .watch();
 
     return (...args: D) => {
       return worker.run(...args);
@@ -30,21 +38,22 @@ export class WeWorker<D extends any[], R> implements IWorker<D, R> {
 
   }
 
-  compile() {
+  public compile() {
     const script = createScript(this.fn);
     const blob = createBlob(script);
     const url = createUrl(blob);
+    //
     this.worker = createWorker(url.url);
+
+    return this;
   }
 
-  watch() {
+  public watch() {
     this.worker.onmessage = ({ data: { id, response, error } }) => {
       // @IGNORE not found task
-      if (!this.tasks[id]) return ;
+      if (!this.hasTask(id)) return ;
 
-      const { resolve, reject } = this.tasks[id];
-      // task only comsume once
-      delete this.tasks[id];
+      const { resolve, reject } = this.useTask(id);
       
       if (error) {
         const _error = new Error(error.message);
@@ -55,20 +64,15 @@ export class WeWorker<D extends any[], R> implements IWorker<D, R> {
 
       return resolve(response);
     };
+
+    return this;
   }
 
-  register(id: string, resolve: any, reject: any) {
-    this.tasks[id] = {
-      resolve,
-      reject,
-    };
-  }
-
-  run(...args: D): Promise<R> {
+  public run(...args: D): Promise<R> {
     return new Promise((resolve, reject) => {
       const id = this.id();
 
-      this.register(id, resolve, reject);
+      this.registerTask(id, resolve, reject);
 
       this.worker.postMessage({
         id,
@@ -76,9 +80,34 @@ export class WeWorker<D extends any[], R> implements IWorker<D, R> {
       });
     });
   }
+
+  public async stop() {
+    return this.worker.terminate();
+  }
+
+  //
+  private registerTask(id: string, resolve: any, reject: any) {
+    this.tasks[id] = {
+      resolve,
+      reject,
+    };
+  }
+
+  private useTask(id: string) {
+    const task = this.tasks[id];
+
+    // task only comsume once
+    delete this.tasks[id];
+
+    return task;
+  }
+
+  private hasTask(id: string) {
+    return id in this.tasks;
+  }
 }
 
-export default WeWorker;
+export default WebWorker;
 
 // utils
 
@@ -146,6 +175,6 @@ var a = async (x, y) => {
   return x + y;
 }
 
-var aw = WeWorker.create(a);
+var aw = WebWorker.create(a);
 aw(1111, 2).then(console.log);
 */
