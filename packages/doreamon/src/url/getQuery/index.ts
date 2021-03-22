4c318d999681a51b5aa456216ab1e49b8b575741
whatwewant <tobewhatwewant@outlook.com>
2021-03-23 01:57:00 +0800

    chore(doreamon): url.getQuery support optional name props

--- packages/doreamon/src/url/getQuery/index.ts
+++ packages/doreamon/src/url/getQuery/index.ts
@@ -1,7 +1,12 @@
 import * as qs from '@zcorky/query-string';
 
-export function getQuery() {
-  return qs.parse(location.search);
+export function getQuery(name?: string) {
+  const query = qs.parse(location.search);
+  if (name) {
+    return query[name];
+  }
+
+  return query;
 }
 
 export default getQuery;
