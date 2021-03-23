e5c204a44a6509d0777b4f56208b18e4432e6440
whatwewant <tobewhatwewant@outlook.com>
2021-03-23 10:06:35 +0800

    chore: add time-ago

---
+++ packages/time-ago/src/constants.ts
@@ -0,0 +1,33 @@
+export const DATA = [
+  {
+    ceil: 1000,
+    locale: 'just now',
+  },
+  {
+    ceil: 60 * 1000,
+    locale: '{n} seconds ago',
+  },
+  {
+    ceil: 60 * 60 * 1000,
+    locale: '{n} minutes ago',
+  },
+  {
+    ceil: 24 * 60 * 60 * 1000,
+    locale: '{n} hours ago',
+  },
+  {
+    ceil: 7 * 24 * 60 * 60 * 1000,
+    locale: '{n} days ago',
+  },
+  {
+    ceil: 28 * 24 * 60 * 60 * 1000,
+    locale: '{n} weeks ago',
+  },
+  {
+    ceil: 12 * 30 * 24 * 60 * 60 * 1000,
+    locale: '{n} months ago',
+  },{
+    ceil: 10* 365 * 24 * 60 * 60 * 1000,
+    locale: '{n} years ago',
+  },
+];
---
+++ packages/time-ago/src/index.ts
@@ -0,0 +1,50 @@
+import { number as isNumber } from '@zcorky/is';
+import i18n, { t } from '@zodash/i18n';
+import { DATA } from './constants';
+
+const zh_CN = require('./locales/zh_CN.json');
+const en_US = require('./locales/en_US.json');
+
+export interface Options {
+  /**
+   * Now Date
+   */
+  now?: Date | number;
+
+  /**
+   * Navigator.language
+   */
+  language?: 'en-US' | 'zh-CN';
+}
+
+i18n.config({
+  locales: {
+    en_US,
+    zh_CN,
+  },
+});
+
+export function timeAgo(ago: Date | number, options?: Options) {
+  const _ago = isNumber(ago) ? new Date(ago) : ago;
+  const _now = isNumber(options?.now) ? new Date(options.now) : options.now;
+
+  const diff = _now.getTime() - _ago.getTime();
+  let i = 0;
+  while ((diff / DATA[i].ceil) > 1) {
+    i++;
+  }
+
+  const _time = i === 0 ? diff : Math.floor(diff / DATA[i - 1].ceil);
+  
+  //
+  if (options?.language) {
+    i18n.setLocale(options.language.replace('-', '_'));
+  } else {
+    i18n.setLocale(window.navigator?.language?.replace('-', '_'))
+  }
+
+  //
+  return t(DATA[i].locale, { n: _time });
+}
+
+export default timeAgo;
