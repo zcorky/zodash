4ae41f707576d04f7fc0e7ae9a5443762875c9ce
whatwewant <tobewhatwewant@outlook.com>
2021-03-24 07:09:51 +0800

    chore(status): fix statues package import path @TODO

--- packages/statuses/__tests__/statuses.test.ts
+++ packages/statuses/__tests__/statuses.test.ts
@@ -1,4 +1,8 @@
-import * as lstatuses from 'statuses'; // @TODO why import from current
+// import * as lstatuses from 'statuses';
+
+// @TODO why import from current
+/* eslint-disable-next-line */
+import * as lstatuses from '../../../node_modules/statuses';
 import * as statuses from '../src/statuses';
 
 describe('@zodash/statuses', () => {
@@ -40,7 +44,7 @@ describe('@zodash/statuses', () => {
 
   it('INTERNAL_SERVER_ERROR === 500', () => {
     expect(lstatuses('INTERNAL SERVER ERROR')).toBe(
-      statuses.INTERNAL_SERVER_ERROR,
+      statuses.INTERNAL_SERVER_ERROR
     );
   });
 
