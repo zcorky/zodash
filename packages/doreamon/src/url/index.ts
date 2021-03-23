6fd63e2f28c4f6ff4f327181d9c4731f9e83cda6
whatwewant <tobewhatwewant@outlook.com>
2021-03-23 12:54:18 +0800

    chore(doreamon): mv string.{urlEncode,urlDecode} to url.{urlEncode,urlDecode}

--- packages/doreamon/src/url/index.ts
+++ packages/doreamon/src/url/index.ts
@@ -1,7 +1,11 @@
 import getUrl from './getUrl';
 import getQuery from './getQuery';
+import urlEncode from './urlEncode';
+import urlDecode from './urlDecode';
 
 export default {
   getUrl,
   getQuery,
+  urlEncode,
+  urlDecode,
 };
