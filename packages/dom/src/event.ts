dbe725f74020f03aaf2ea768b807689d48b8dbb5
Zero <tobewhatwewant@outlook.com>
2021-04-30 14:10:38 +0800

    chore(dom): add event.delegate in capture phage

--- packages/dom/src/event.ts
+++ packages/dom/src/event.ts
@@ -12,3 +12,15 @@ export function on($el: El | Window, eventName: string, listener: Listener) {
 export function off($el: El | Window, eventName: string, listener: Listener) {
   $($el).removeEventListener(eventName, listener);
 }
+
+export function delegate(
+  $el: El | Window,
+  eventName: string,
+  listener: Listener
+) {
+  $($el).addEventListener(eventName, listener, true);
+
+  return function _off() {
+    $($el).removeEventListener(eventName, listener, true);
+  };
+}
