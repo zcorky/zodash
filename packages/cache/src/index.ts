4c495a092f2b46cb974712a751a3a49922c2b6a7
Zero <tobewhatwewant@outlook.com>
2021-05-02 23:40:47 +0800

    chore(cache): add capacity for static create method

--- packages/cache/src/index.ts
+++ packages/cache/src/index.ts
@@ -1,12 +1,12 @@
 import LRU, { LRU as ILRU } from '@zcorky/lru';
 
-export type ICache<K, V> = ILRU<K, V>
+export type ICache<K, V> = ILRU<K, V>;
 
 export class Cache<K = string, V = any>
   extends LRU<K, V>
   implements ICache<K, V> {
-  public static create<K = string, V = any>() {
-    return new Cache<K, V>();
+  public static create<K = string, V = any>(capacity?: number) {
+    return new Cache<K, V>(capacity);
   }
 }
 
