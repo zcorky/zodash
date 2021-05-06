fb064b42887a1242ae2b75ac2e7c7e69c60e8e5a
Zero <tobewhatwewant@outlook.com>
2021-05-06 21:48:23 +0800

    fix(crypto): export index

---
+++ packages/crypto/src/index.ts
@@ -0,0 +1,18 @@
+// encode/decode
+export * as base64 from './base64';
+
+// hash
+export { md5 } from './md5';
+export { sha1 } from './sha1';
+export { sha256 } from './sha256';
+export { sha512 } from './sha512';
+
+// hash with salt
+export * as hmac from './hmac';
+
+// crypto/decryto
+export * as aes from './aes';
+export * as rsa from './rsa';
+
+// data singature
+export { signature } from './signature';
