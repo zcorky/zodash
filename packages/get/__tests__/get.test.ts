58053cd314b5fafcd5d90e2859b066b5bf0d5093
whatwewant <tobewhatwewant@outlook.com>
2021-09-18 12:34:52 +0800

    feat(get): support template literal Types in TypeScript v4.1

--- packages/get/__tests__/get.test.ts
+++ packages/get/__tests__/get.test.ts
@@ -8,15 +8,17 @@ describe('@zodash/get', () => {
     const object = { a: { b: [{ c: { d: 1, e: false } }] } };
 
     const v1 = lget(object, 'a.b[0].c.d');
-    const v2 = get(object, 'a.b.0.c.d');
+    const v2 = get(object, 'a.b[0].c.d');
+    const v2_1 = get(object, 'a.b.0.c.d');
 
     const v3 = lget(object, 'a.c[0].c.d', null);
-    const v4 = get(object, 'a.c.0.c.d', null);
+    const v4 = get(object, 'a.c.[0].c.d', null);
 
     const v5 = lget(object, 'a.c.0.c.d.e');
     const v6 = get(object, 'a.c.0.c.d.e') as any;
 
     expect(deepEqual(v1, v2 as any)).toBeTruthy();
+    expect(deepEqual(v1, v2_1 as any)).toBeTruthy();
     expect(deepEqual(v3, v4)).toBeTruthy();
     expect(deepEqual(v5, v6)).toBeTruthy();
   });
--- packages/get/src/get.ts
+++ packages/get/src/get.ts
@@ -2,6 +2,51 @@ import { toPath } from '@zodash/to-path';
 import { undefined as isUndef, array as isArray } from '@zcorky/is';
 
 /**
+ * Template Literal Types
+ *  [v4.1, released in Nov 2020](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-1.html#template-literal-types)
+ *
+ * References:
+ *  1. https://dev.to/tipsy_dev/advanced-typescript-reinventing-lodash-get-4fhe
+ *  2. https://dev.to/pouja/making-lodash-function-get-type-safe-313o
+ */
+type GetIndexedField<T, K> = K extends keyof T
+  ? T[K]
+  : K extends `${number}`
+  ? '0' extends keyof T
+    ? undefined
+    : number extends keyof T
+    ? T[number]
+    : undefined
+  : undefined;
+
+type FieldWithPossiblyUndefined<T, Key> =
+  | GetFieldType<Exclude<T, undefined>, Key>
+  | Extract<T, undefined>;
+
+type IndexedFieldWithPossiblyUndefined<T, Key> =
+  | GetIndexedField<Exclude<T, undefined>, Key>
+  | Extract<T, undefined>;
+
+export type GetFieldType<T, P> = P extends `${infer Left}.${infer Right}`
+  ? Left extends keyof T
+    ? FieldWithPossiblyUndefined<T[Left], Right>
+    : Left extends `${infer FieldKey}[${infer IndexKey}]`
+    ? FieldKey extends keyof T
+      ? FieldWithPossiblyUndefined<
+          IndexedFieldWithPossiblyUndefined<T[FieldKey], IndexKey>,
+          Right
+        >
+      : undefined
+    : undefined
+  : P extends keyof T
+  ? T[P]
+  : P extends `${infer FieldKey}[${infer IndexKey}]`
+  ? FieldKey extends keyof T
+    ? IndexedFieldWithPossiblyUndefined<T[FieldKey], IndexKey>
+    : undefined
+  : undefined;
+
+/**
  * Get the value at path of object.
  * If the resolved values is undefined, the defaultValue is returnted in its place.
  *
@@ -9,25 +54,21 @@ import { undefined as isUndef, array as isArray } from '@zcorky/is';
  * @param path The path of the property to get.
  * @param defaultValue The value returned for undefined resolved values.
  */
-export function get<T extends object, D = any>(
-  value: T,
-  path: string,
-  defaultValue: D = undefined,
-): D {
-  // return toPath(path)
-  //   .reduce((p, k) => {
-  //     if (p && typeof p[k] !== 'undefined') {
-  //       return p[k];
-  //     }
-
-  //     return defaultValue;
-  //   }, value);
+export function get<
+  Obj extends object,
+  Path extends string,
+  Default = GetFieldType<Obj, Path>,
+>(
+  value: Obj,
+  path: Path,
+  defaultValue?: Default,
+): GetFieldType<Obj, Path> | Default {
   const _v = getValue(value, toPath(path));
 
   return !isUndef(_v) ? _v : defaultValue;
 }
 
-export function getValue(
+function getValue(
   parent: object | object[],
   paths: string[],
   currentIndex = 0,
@@ -55,5 +96,7 @@ export function getValue(
     return parent;
   }
 
-  return (parent as any).map((v: any) => getValue(v, paths, currentIndex + 1));
+  return (parent as any).map((v: any) =>
+    getValue(v, paths, currentIndex + 1),
+  );
 }
