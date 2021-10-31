c93bd0d2f36b091608dada6cc30e9910821638f3
whatwewant <tobewhatwewant@outlook.com>
2021-10-31 10:11:08 +0800

    chore(timeout): support option message

--- packages/timeout/src/index.ts
+++ packages/timeout/src/index.ts
@@ -4,14 +4,21 @@ export interface IOptions {
    *  default: 3000
    */
   ms?: number;
+
+  /**
+   * Custom Timeout Error Message
+   */
+  message?: string;
 }
 
 const DEFAULT_TIMEOUT = 3000;
-const ERROR = new Error('timeout');
+const DEFAULT_TIMEOUT_ERROR_MESSAGE = 'timeout';
 
-function exception(ms: number) {
+function exception(ms: number, message?: string) {
+  const error = new Error(message ?? DEFAULT_TIMEOUT_ERROR_MESSAGE);
+
   return new Promise((_resolve, reject) => {
-    setTimeout(() => reject(ERROR), ms);
+    setTimeout(() => reject(error), ms);
   });
 }
 
@@ -23,18 +30,18 @@ function toPromise<R>(fnOrPromise: Promise<R> | (() => Promise<R>)) {
 
 export async function timeout<R = any>(
   fn: () => Promise<R>,
-  options?: IOptions
+  options?: IOptions,
 ): Promise<R>;
 export async function timeout<R = any>(
   fn: Promise<R>,
-  options?: IOptions
+  options?: IOptions,
 ): Promise<R>;
 export async function timeout<R = any>(
   fnOrPromise: any,
-  options?: IOptions
+  options?: IOptions,
 ): Promise<R> {
   const ms = options?.ms ?? DEFAULT_TIMEOUT;
-  const exceptionPromise = exception(ms);
+  const exceptionPromise = exception(ms, options.message);
   const taskPromise = toPromise(fnOrPromise);
 
   return Promise.race([exceptionPromise, taskPromise]);
