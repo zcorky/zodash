9aff98add49c123c34da1ea9d3261796d75f5815
whatwewant <tobewhatwewant@outlook.com>
2021-11-16 01:39:30 +0800

    chore: add cases (camel + snake + kebab)

---
+++ packages/cases/__tests__/index.test.ts
@@ -0,0 +1,7 @@
+// import * as timer from '../src';
+
+describe('@zodash/cases', () => {
+  it('run', async () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ packages/cases/src/index.ts
@@ -0,0 +1,3 @@
+export * from '@zodash/camel-case';
+export * from '@zodash/snake-case';
+export * from '@zodash/kebab-case';
