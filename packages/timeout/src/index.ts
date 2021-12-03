4a41404d2c6307e7589c070b780caf7d12c8ecd2
whatwewant <tobewhatwewant@outlook.com>
2021-12-03 13:33:36 +0800

    fix(timeout): should clearTimeout or it will hold in background

--- packages/timeout/src/index.ts
+++ packages/timeout/src/index.ts
@@ -14,13 +14,13 @@ export interface IOptions {
 const DEFAULT_TIMEOUT = 3000;
 const DEFAULT_TIMEOUT_ERROR_MESSAGE = 'timeout';
 
-function exception(ms: number, message?: string) {
-  const error = new Error(message ?? DEFAULT_TIMEOUT_ERROR_MESSAGE);
+// function exception(ms: number, message?: string) {
+//   const error = new Error(message ?? DEFAULT_TIMEOUT_ERROR_MESSAGE);
 
-  return new Promise((_resolve, reject) => {
-    setTimeout(() => reject(error), ms);
-  });
-}
+//   return new Promise((_resolve, reject) => {
+//     setTimeout(() => reject(error), ms);
+//   });
+// }
 
 function toPromise<R>(fnOrPromise: Promise<R> | (() => Promise<R>)) {
   return typeof fnOrPromise === 'function'
@@ -41,10 +41,19 @@ export async function timeout<R = any>(
   options?: IOptions,
 ): Promise<R> {
   const ms = options?.ms ?? DEFAULT_TIMEOUT;
-  const exceptionPromise = exception(ms, options?.message);
+  const error = new Error(options?.message ?? DEFAULT_TIMEOUT_ERROR_MESSAGE);
+  let it: NodeJS.Timeout = null;
+
+  const exceptionPromise = new Promise((_resolve, reject) => {
+    it = setTimeout(() => reject(error), ms);
+  });
   const taskPromise = toPromise(fnOrPromise);
 
-  return Promise.race([exceptionPromise, taskPromise]);
+  const res = await Promise.race([exceptionPromise, taskPromise]);
+  clearTimeout(it);
+  it = null;
+
+  return res;
 }
 
 export default timeout;
