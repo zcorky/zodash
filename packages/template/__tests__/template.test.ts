1eb31d6ef90a4aa7a60b5d7c6d41707ee961aeb1
whwewant <tobewhatwewant@outlook.com>
2022-05-28 15:22:53 +0800

    fix(template): test ok

--- packages/template/__tests__/template.test.ts
+++ packages/template/__tests__/template.test.ts
@@ -17,24 +17,26 @@ describe('@zodash/template', () => {
   it('Custom Seperator {{x}}', () => {
     const pattern = 'Author: {{author}}, From: {{from}}, {{NOTEXIST}}';
     const map = { author: 'Zero', from: 'China' };
-    expect(template(pattern, map, { start: '{{', end: '}}' })).toBe(
-      'Author: Zero, From: China, ',
-    );
+    expect(
+      template(pattern, map, { seperator: { start: '{{', end: '}}' } }),
+    ).toBe('Author: Zero, From: China, ');
   });
 
   it('Custom Seperator #x#', () => {
     const pattern = 'Author: #author#, From: #from#, #NOTEXIST#';
     const map = { author: 'Zero', from: 'China' };
-    expect(template(pattern, map, { start: '#', end: '#' })).toBe(
-      'Author: Zero, From: China, ',
-    );
+    expect(
+      template(pattern, map, { seperator: { start: '#', end: '#' } }),
+    ).toBe('Author: Zero, From: China, ');
   });
 
   it('map function', () => {
     const pattern = 'Author: {author}, From: {from}, GirlFriend: {girl.name}';
     const map = { author: 'Zero', from: 'China', girl: { name: 'any' } };
     expect(
-      template(pattern, (key) => get(map, key), // map[key];
+      template(
+        pattern,
+        (key) => get(map, key), // map[key];
       ),
     ).toBe('Author: Zero, From: China, GirlFriend: any');
   });
