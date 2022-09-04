57a904b1b1e6f7c9c941f014d753fae3288e92e3
whatwewant <tobewhatwewant@outlook.com>
2022-09-04 13:36:18 +0800

    chore(dom): router add parse + isMatch

--- packages/dom/__tests__/index.test.ts
+++ packages/dom/__tests__/index.test.ts
@@ -1,7 +1,26 @@
-import timer from '../src';
+import * as router from '../src/router';
 
-describe('@zodash/timer', () => {
-  it('run', async () => {
+describe('@zodash/dom', () => {
+  it('works', async () => {
     expect(true).toBe(true);
+  });
+});
+
+describe('@zodash/dom#router', () => {
+  it('parse', async () => {
+    expect(router.parse('/user/:id/:name', '/user/1/zodash')).toEqual({
+      id: '1',
+      name: 'zodash',
+    });
+
+    expect(router.parse('/user', '/user')).toEqual({});
+
+    expect(router.parse('/user/:id/:name', '/userx/1/zodash')).toEqual(null);
+  });
+
+  it('isMatch', async () => {
+    expect(router.isMatch('/user/:id/:name', '/user/1/zodash')).toBeTruthy();
+    expect(router.isMatch('/user/:id/:name', '/users/1/zodash')).toBeFalsy();
+    expect(router.isMatch('/user/:id/:name', '/user/1/zodash/x')).toBeFalsy();
   });
 });
--- packages/dom/src/router.ts
+++ packages/dom/src/router.ts
@@ -1,11 +1,12 @@
-import { parse } from '@zcorky/query-string';
+import * as qs from '@zcorky/query-string';
+import * as pathToRegexp from 'path-to-regexp';
 
 export function getUrl(): string {
   return window.location.href;
 }
 
 export function getQuery(): Record<string, string> {
-  return parse(window.location.search);
+  return qs.parse(window.location.search);
 }
 
 export function getLocation() {
@@ -22,4 +23,29 @@ export function go(url: string, canGoBack = true) {
 
 export function navigate(url: string, canGoBack = true) {
   return go(url, canGoBack);
+}
+
+export function parse(
+  regStr: string,
+  pathname: string,
+): Record<string, string> | null {
+  const regexp = pathToRegexp(regStr);
+  const result = regexp.exec(pathname);
+  if (!result) {
+    return null;
+  }
+
+  const keys: pathToRegexp.Key[] = (regexp as any).keys ?? [];
+  if (!keys.length) {
+    return {};
+  }
+
+  return keys.reduce((acc, key, index) => {
+    acc[key.name] = result[index + 1];
+    return acc;
+  }, {});
+}
+
+export function isMatch(regStr: string, pathname: string): boolean {
+  return !!parse(regStr, pathname);
 }
